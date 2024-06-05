e2db2a498a9cbe41150849ac64f57424b9fe4445
mll-enh <marcco.laskowski@gmail.com>
2024-06-05 15:44:50 -0400

    update :3

--- bin/assets/custom_events/Camera Flash.lua
+++
@@ -1,6 +0,0 @@
-function onEvent(name, value1, value2)
-
-    if name == "Camera Flash" then
-		cameraFlash('camother', '' .. value1, value2, true)
-	end
-end
--- bin/assets/data/Konton-Boogie/script.lua
+++
@@ -1,9 +0,0 @@
-
-function onCreate()
-    setProperty("scoreTxt.y", getProperty('scoreTxt.y') -50)
-    setProperty('healthBar.alpha', 0)
-    setProperty('iconP1.alpha', 0)
-    setProperty('iconP2.alpha', 0)
-    setTextFont('scoreTxt','pixel.otf')
-    setTextSize('scoreTxt',18)
-end
