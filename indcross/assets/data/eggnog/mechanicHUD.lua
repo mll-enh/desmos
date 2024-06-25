5f175f2982f0a00bca9ab53107e662f73a4708a7
mll-enh <64047643+mll-enh@users.noreply.github.com>
2024-06-25 11:45:10 -0400

    delete bad indie cross

--- indcross/assets/data/eggnog/mechanicHUD.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-  --background
-  makeLuaSprite('front','cup/attack',-600,200)
-  setScrollFactor('front',0.0,0.0)
-  scaleObject('front',1.0,1.0)
-  addLuaSprite('front',true)
-close(true);
-end
